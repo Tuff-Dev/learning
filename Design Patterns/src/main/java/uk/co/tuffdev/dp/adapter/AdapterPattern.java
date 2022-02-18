17073aa4eeacae5cd78fd7a3462d565294320c02
Will Tuffin <tuff2dev@gmail.com>
2022-02-18 12:04:53 +0000

    Design patterns project

---
+++ Design Patterns/src/main/java/uk/co/tuffdev/dp/adapter/AdapterPattern.java
@@ -0,0 +1,30 @@
+package uk.co.tuffdev.dp.adapter;
+
+class Point {
+    public int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
+class Line {
+    public Point start, end;
+
+    public Line(Point start, Point end) {
+        this.start = start;
+        this.end = end;
+    }
+}
+
+public class AdapterPattern {
+}
